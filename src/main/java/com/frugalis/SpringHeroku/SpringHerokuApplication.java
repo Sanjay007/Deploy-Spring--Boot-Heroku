d2ae636cea19a5a899161f95ec6180931aa57550
Sanjay Yadav <syadav2@yodlee.com>
2018-05-15 01:33:22 +0530

    initial commit:

---
+++ src/main/java/com/frugalis/SpringHeroku/SpringHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.frugalis.SpringHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuApplication.class, args);
+	}
+}
